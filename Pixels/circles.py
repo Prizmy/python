d24121f87016c24700beed4142c23425f9b6dac9
Fakyring <darkveyron@mail.ru>
2023-02-14 20:28:06 +0300

    Added circle

---
+++ Pixels/circles.py
@@ -0,0 +1,25 @@
+import math
+import tkinter as tk
+
+
+def pyshader(func, w, h):
+    scr = bytearray((0, 0, 0) * w * h)
+    for y in range(h):
+        for x in range(w):
+            p = (w * y + x) * 3
+            scr[p:p + 3] = [max(min(int(c * 255), 255), 0)
+                            for c in func(x / w, y / h)]
+    return bytes('P6\n%d %d\n255\n' % (w, h), 'ascii') + scr
+
+
+def func(x, y):
+    def circle(shift):
+        return 0.1 - (x - (0.5 + shift)) ** 2 - (y - (0.5 + shift)) ** 2
+    return 10 * circle(0.03), 10 * circle(-0.03), 0
+    
+
+label = tk.Label()
+img = tk.PhotoImage(data=pyshader(func, 256, 256)).zoom(2, 2)
+label.pack()
+label.config(image=img)
+tk.mainloop()
