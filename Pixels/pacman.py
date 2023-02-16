a3bc0395fa52ff2fcaa8f73798ca63aa8c1ae20f
Fakyring <darkveyron@mail.ru>
2023-02-16 18:13:17 +0300

    Added pacman

---
+++ Pixels/pacman.py
@@ -0,0 +1,31 @@
+import math
+import tkinter as tk
+
+
+def pyshader(func, w, h):
+    scr = bytearray((0, 0, 0) * w * h)
+    for y in range(h):
+        for x in range(w):
+            p = (w * y + x) * 3
+            scr[p:p + 3] = [max(min(int(c * 255), 255), 0)
+                            for c in func(x / w, y / h)]
+    return bytes('P6\n%d %d\n255\n' % (w, h), 'ascii') + scr
+
+
+def func(x, y):
+    cl = 0
+    cl = 0.1 - (x - 0.5) ** 2 - (y - 0.5) ** 2
+    if cl > 0:
+        cl = 255
+    if (0.003 - (x - 0.6) ** 2 - (y - 0.3) ** 2) > 0:
+        cl = 0
+    if (y>=0.5 and x - y > 0) or (y < 0.5 and x + y >= 1):
+        cl = 0
+    return cl, cl, 0
+    
+
+label = tk.Label()
+img = tk.PhotoImage(data=pyshader(func, 256, 256)).zoom(2)
+label.pack()
+label.config(image=img)
+tk.mainloop()
